3eccafa3a10790d9f19d8fb2aa1a618a13c4d45b
SaiRahul404 <rahul.gajawada@wavemaker.com>
2025-07-10 21:00:15 +0530

    9th July

--- src/main/java/org/example/InterfaceExample.java
+++ src/main/java/org/example/InterfaceExample.java
@@ -29,10 +29,12 @@ interface Swimmable {
 }
 
 class Duck implements Flyable, Swimmable {
+    @Override
     public void fly() {
         System.out.println("Duck is flying");
     }
 
+   @Override
     public void swim() {
         System.out.println("Duck is swimming");
     }
